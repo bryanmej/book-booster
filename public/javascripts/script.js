fe268470a104bc756c3d9dbb7a14f6ae033b0926
bryanmej <bryanmej@gmail.com>
2019-07-09 12:14:17 -0500

    profile styling

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+const btn = document.querySelector('.btn')
+const form = document.querySelector('.prof-form')
+const instruct = document.querySelector('.instruct')
+
+btn.onclick = () => {
+  form.classList.toggle('display')
+  instruct.classList.toggle('display')
+}
